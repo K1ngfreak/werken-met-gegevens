80bcd02463e71de146c855a1f004f493d2f8d0e4
Kingfreak <noahzonneveld@hotmail.com>
2021-09-08 12:02:36 +0200

    berekening

---
+++ feestluch.py
@@ -0,0 +1,5 @@
+croissant = 0.39
+stokbrood = 2.78
+korting = 0.50
+
+print(17 * croissant + 2 * stokbrood - 3 * korting)
---
+++ speelhaldag.py
@@ -0,0 +1,5 @@
+toegang = 7.45
+vijf_minuten = 0.37
+totaal_minuten = 27
+
+print((toegang + (9 * vijf_minuten)) * 3)
